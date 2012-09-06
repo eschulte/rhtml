1969ee8f88217516fe89db08098fc93757529d88
Jose Peleteiro <jose@peleteiro.net>
2012-09-06 07:33:17 -0300

    Fix extract-partial incorret partial buffer name

--- rhtml-mode.el
+++ rhtml-mode.el
@@ -3,9 +3,9 @@
 ;; ***** BEGIN LICENSE BLOCK *****
 ;; Version: MPL 1.1/GPL 2.0/LGPL 2.1
 
-;; The contents of this file are subject to the Mozilla Public License Version 
-;; 1.1 (the "License"); you may not use this file except in compliance with 
-;; the License. You may obtain a copy of the License at 
+;; The contents of this file are subject to the Mozilla Public License Version
+;; 1.1 (the "License"); you may not use this file except in compliance with
+;; the License. You may obtain a copy of the License at
 ;; http://www.mozilla.org/MPL/
 
 ;; Software distributed under the License is distributed on an "AS IS" basis,
@@ -61,7 +61,7 @@
 (defun extract-partial (begin end partial-name)
   (interactive "r\nsName your partial: ")
   (kill-region begin end)
-  (find-file (concat "_" partial-name "\\.html\\.erb"))
+  (find-file (concat "_" partial-name ".html.erb"))
   (yank)
   (pop-to-buffer nil)
   (insert (concat "<%= render :partial => '" partial-name "' %>\n")))
